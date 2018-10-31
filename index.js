707b2746fe47825747c7b9fb19e8f4bbbb312326
iDevestate <44614007+iDevestate@users.noreply.github.com>
2018-10-31 11:54:02 +0100

    s

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const botconfig = require("./botconfig.json");
-const tokenfile = require("./token.json");
+//const tokenfile = require("./token.json");
 const Discord = require("discord.js");
 const fs = require("fs");
 const token = process.env.token;
