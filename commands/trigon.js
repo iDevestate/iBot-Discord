930a3626fb98dfe22fa086333adeb2cb1e91ebc8
iDevestate <44614007+iDevestate@users.noreply.github.com>
2018-11-02 09:52:19 +0100

    new trigon.js

---
+++ commands/trigon.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+   message.reply("Trigon download at https://arponag.xyz/trigon/");
+}
+
+module.exports.help = {
+    name: "trigon"
+}
