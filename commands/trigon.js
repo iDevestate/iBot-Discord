e26b9d6e5af66e65d1f3c44491bdfc1b88f9294b
iDevestate <44614007+iDevestate@users.noreply.github.com>
2018-11-02 09:54:51 +0100

    updated trigon

--- commands/trigon.js
+++ commands/trigon.js
@@ -1,7 +1,11 @@
 const Discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) => {
+    let botChannel = message.guild.channels.find(`name`, "bot_commands");
+    if (!botChannel) return;
    message.reply("Trigon download at https://arponag.xyz/trigon/");
+
+   
 }
 
 module.exports.help = {
