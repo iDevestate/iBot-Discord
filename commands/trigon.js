19ea625423bdd8639dc74cf72c00b7c290a7c488
iDevestate <44614007+iDevestate@users.noreply.github.com>
2018-11-03 11:42:42 +0100

    First new commit

---
+++ commands/trigon.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+   message.reply("Trigon can be downloaded from http://arponag.xyz/");
+}
+
+module.exports.help = {
+    name: "trigon"
+}
